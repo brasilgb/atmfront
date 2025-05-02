9ac96b562487f9f6bbc771c5e47cc38d60df155b
Anderson .Brasil <andersonbrasil72@gmail>
2025-05-02 17:45:15 -0300

    Push

--- src/app/(customer-routes)/customer/invoicing/page.tsx
+++ src/app/(customer-routes)/customer/invoicing/page.tsx
@@ -1,6 +1,21 @@
+import { getServerSession } from 'next-auth'
+import { nextAuthOptions } from '@/app/api/auth/[...nextauth]/route'
 import React from 'react'
 
-export default function Invoicing() {
+export default async function Invoicing() {
+
+  const session = await getServerSession(nextAuthOptions);
+
+  let data = await fetch('http://localhost:3000/data-sale?org_id=77544b4c-1cc9-4751-8c2e-21226ae497b6&comp_id=0002', {
+    method: 'GET',
+    headers: {
+      'Content-type': 'Application/json',
+      Authorization: `Bearer ${session?.user?.token}`
+    }
+  });
+  let sales = await data.json()
+  console.log(sales);
+
   return (
     <div>Invoicing</div>
   )
